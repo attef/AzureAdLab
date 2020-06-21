16fe20b3b483dc81d4c30786fae3efdd58a13b47
attef touina <attef_euler@hotmail.com>
2020-06-21 12:14:50 +0200

    msal config

---
+++ SPA-JS/src/common.js
@@ -0,0 +1,14 @@
+const maslConfig = {
+    auth : {
+        "clientId" : "1dfbb6bc-139e-4db2-9b15-81ab0b581fbd",
+        "authority": "https://login.microsoftonline.com/8240eed4-15d9-4e26-b437-49d30ac61009",
+        "redirectUri" : "https://localhost:3000"
+    },
+    cache : {
+        "cacheLocation" : "sessionStorage"
+    }
+}
+
+var userAgentApplication = new Msal.UserAgentApplication(maslConfig);
+
+
