9319b6d100e76744292c64a57442afc3a04fc9c1
attef touina <atif_euler@hotmail.com>
2020-06-30 07:41:54 +0200

    Acquire and use access token in popup mode

--- SPA-JS/src/authRedirect.js
+++ SPA-JS/src/authRedirect.js
@@ -29,44 +29,4 @@ function getAccessToken(requestParams, callApi){
         })
 }
 
-const apiScopes = [
-    "api://4dbf2a2e-197e-4e51-8cb2-2fcd27554aa3/Todo.Add",
-    "api://4dbf2a2e-197e-4e51-8cb2-2fcd27554aa3/Todo.Read",
-    "api://4dbf2a2e-197e-4e51-8cb2-2fcd27554aa3/Todo.ReadAll"
-];
-
-function requestApi(endpoint, method, accessToken) {
-    const headers = new Headers();
-    headers.append("Authorization", "bearer "+ accessToken);
-    return fetch(endpoint, {
-        headers : headers,
-        method : method
-    })
-    .then(function (response) {
-        return response.json();
-    })
-}
-
-function getMyTodos() {
-    getAccessToken({scopes: apiScopes}, function (accessToken) {
-        requestApi("https://localhost:5001/todo/myTodos", "GET", accessToken)
-            .then(function (todos) {
-                UI.showTodos(todos)
-            });
-    })
-}
-
-function AddTodo() {
-    var titleInput = $("#todoTitle");
-    getAccessToken({scopes : apiScopes}, function (accessToken) {
-        requestApi("https://localhost:5001/todo/add?title="+titleInput.val(), "POST",
-        accessToken)
-        .then(function (todo) {
-            UI.addTodo(todo);
-            titleInput.val("");
-        })
-    })
-}
-
-
 getMyTodos();
